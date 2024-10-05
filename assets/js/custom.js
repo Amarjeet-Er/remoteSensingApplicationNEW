3d3696da51235053ff37b3b48fd3dfa29d998251
theayushkumar <ayushkumarhjp@gmail.com>
2024-10-05 16:52:33 +0530

    gallery done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -159,6 +159,27 @@
 				items: 2
 			},
 			1000: {
+				items: 6
+			}
+		}
+	})
+
+	// for gallery
+	$('.owl-gallery-item').owlCarousel({
+		items: 4,
+		loop: true,
+		dots: true,
+		nav: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
 				items: 4
 			}
 		}
