75c534dd64af694aa0d640f70e0eadfaaaac1f22
Amarjeet Kumar <bidupur8544@gmail.com>
2024-10-05 18:23:04 +0530

    final

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -184,7 +184,28 @@
 			}
 		}
 	})
-
+	// for scientists
+	$('.owl-scientists-item').owlCarousel({
+		items: 4,
+		loop: true,
+		dots: true,
+		// nav: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  600: {
+			items: 2
+		  },
+		  1000: {
+			items: 4
+		  }
+		}
+	  });
+	  
+	  
 
 	// Menu Dropdown Toggle
 	if ($('.menu-trigger').length) {
